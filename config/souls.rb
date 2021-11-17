319d499107fdf5e02088afaff2b01c32d4ec9db9
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2021-11-17 22:36:56 +0100

    first commit

---
+++ config/souls.rb
@@ -0,0 +1,9 @@
+Souls.configure do |config|
+  config.app = "ruby-conf"
+  config.project_id = "ruby-conf"
+  config.region = "asia-northeast1"
+  config.endpoint = "/endpoint"
+  config.strain = "mother"
+  config.fixed_gems = ["spring"]
+  config.workers = []
+end
